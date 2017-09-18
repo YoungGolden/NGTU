d42ba52fc227ba22c5a357f6c3fda5f88f083ff3
YoungGolden <31438864+YoungGolden@users.noreply.github.com>
2017-09-19 00:00:11 +0300

    Update lesson.cpp

--- lessoncpp/lesson.cpp
+++ lessoncpp/lesson.cpp
@@ -6,10 +6,10 @@
 
 using namespace std;
 
-void fup (double x, double x_1, double x_2, double y_1, double y_2){//функция вычисления y
+void fup (double x, double x_1, double x_2, double y_1, double y_2){//fup y
     double y;
     y=((x-x_1)*(y_2-y_1)/(x_2-x_1))+y1;
-    cout << " y>>> " << y << endl;
+    cout << "y>>> " << y << endl;
 }
 
 int main(){
@@ -20,8 +20,8 @@ int main(){
     cout << "x>>>" << endl;
 
         cin >> x;
-        if(x < 0 || x > 160){
-            cout << "Errore" << endl;
+        if(x < 0 || x > 200){
+            cout << ">>>Errore" << endl;
             return 0;
         };
 
@@ -29,28 +29,28 @@ int main(){
     coord.open("text.txt");
 
     if(!coord){
-        cout << "Errore" <<endl;
+        cout << ">>>Errore" <<endl;
         return 0;
         }
-    else{
-        f >> x_2 >> y_2;
-        while(!coord.eof()) {
-            if(x_2 < x){
-            x_1 = x_2;
-            y_1 = y_2;
-            coord >> x_2 >> y_2;
+        else{
+            f >> x_2 >> y_2;
+            while(!coord.eof()) {
+                if(x_2 < x){
+                x_1 = x_2;
+                y_1 = y_2;
+                coord >> x_2 >> y_2;
+                }
+                else if(x_2 == x){
+                    cout << y_2 << endl;
+                    f.close();
+                    return 0;
+                }
+                else {
+                    fup (x, x_1, x_2, y_1, y_2);
+                    coord.close();
+                    return 0;
+                }
             }
-            else if(x_2 == x){
-                cout << y_2 << endl;
-                f.close();
-                return 0;
-            }
-            else {
-                fup (x, x_1, x_2, y_1, y_2);
-                coord.close();
-                return 0;
-            }
-        }
     }
     coord.close();
     return 0;
